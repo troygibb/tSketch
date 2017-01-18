ae5aef0e49d50bd1abc3bda8b8501bf24f51ffdb
chris <cmac2992@gmail.com>
2017-01-18 17:27:55 -0500

    navbar with progress

--- client/Navbar.jsx
+++ client/Navbar.jsx
@@ -9,9 +9,17 @@ class Navbar extends React.Component {
 			<div>
 				<div id="navbar" >
 					<div className='container'>
-							<a href="#">
+							<a className="logo" href="#">
 				        DrawTrumpADoodle!
 				      </a>
+							<div className="checkoutProgress">
+								<span className="checkoutProgress__label">Progress</span>
+								<div className="progress">
+								  <div className="progress-bar">
+								    <span>20%</span>
+								  </div>
+								</div>
+							</div>
 					</div>
 			  </div>
 				{ this.props.children }
