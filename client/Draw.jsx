e76ad96abbaedd7f126c2bc1e72aac0599381797
Andrew Linfoot <ajlin500@yahoo.com>
2017-01-17 16:38:17 -0800

    fix imports

--- client/Draw.jsx
+++ client/Draw.jsx
@@ -1,8 +1,8 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import AT from './lib/atrament';
-import _ from 'lodash';
 
 import { assignAtrament, changeAtramentOption, savePostcardImage } from './actions/index';
 import AtramentOptions from './AtramentOptions';
