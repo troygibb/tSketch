bd0519f97e7ef952ec477b232ca8e78709338068
Andrew Linfoot <ajlin500@yahoo.com>
2017-01-18 22:02:55 -0800

    delay loading doodle thumbnails to wait for lob

--- client/Success.jsx
+++ client/Success.jsx
@@ -3,29 +3,50 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 class Success extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div id='home'>
-				<h1>Success!</h1>
-				<p>
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadingThumbnails: true,
+    };
+  }
+  componentDidMount() {
+    // Delay showing thumbnails for 1 second so the can load on Lob.
+    setTimeout(() => {
+      this.setState({
+        loadingThumbnails: false,
+      });
+    }, 6000);
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Success!</h1>
+        <p>
           Your Doodle should arrive on {this.props.expectedDeliveryDate}.
-				</p>
+        </p>
         <div className="row">
           <div className="col-md-6">
-            <img src={this.props.frontThumbnail} />
+            <div className="success__thumbnail">
+              { this.state.loadingThumbnails ?
+                (<div>Loading Indicator Here</div>) :
+                (<img alt="Front Thumbnail" src={this.props.frontThumbnail} />)
+              }
+            </div>
           </div>
           <div className="col-md-6">
-            <img src={this.props.backThumbnail} />
+            <div className="success__thumbnail">
+              { this.state.loadingThumbnails ?
+                (<div>Loading Indicator Here</div>) :
+                (<img alt="Back Thumbnail" src={this.props.backThumbnail} />)
+              }
+            </div>
           </div>
         </div>
-			</div>
-		)
-	}
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => {
-	return state.orderResponse;
+  return state.orderResponse;
 };
 export default connect(mapStateToProps)(Success);
