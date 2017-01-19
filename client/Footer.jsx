89938b85edbfe2c6b6fe377d7a2dc62bec8a998e
Andrew Linfoot <ajlin500@yahoo.com>
2017-01-18 21:35:37 -0800

    add Layout component

--- client/Footer.jsx
+++ client/Footer.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
 class Footer extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className="footer__wrapper">
-				<a href="https://github.com/Greivancers/tSketch" target="_blank">
-					<img className="footer__github" src="http://www.iconsplace.com/icons/preview/white/github-256.png"/>
-					Github
-				</a>
-				{ this.props.children }
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="footer__wrapper">
+        <a
+          href="https://github.com/Greivancers/tSketch"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="footer__github"
+            role="presentation"
+            src="http://www.iconsplace.com/icons/preview/white/github-256.png"
+          />
+          Github
+        </a>
+        { this.props.children }
+      </div>
+    );
+  }
 }
 
 export default Footer;
